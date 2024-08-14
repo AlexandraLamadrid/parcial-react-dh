418fa93e30701612e11e43c71cdd6622e72db873
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-08-14 16:40:39 -0500

    Subiendo primera parte del parcial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./componentes/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+
+      <form></form>
+
+      <Card />
+    </div>
+  );
+}
+
+export default App;
